7e13156206e1828a61f7963bf15fabd56264111d
Edwin Elias Alvarez <elias986@yahoo.com>
2017-10-08 21:48:29 -0700

    Creación de Interface Icrud

---
+++ libAccesoBD/Icrud.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libAccesoBD
+{
+    public interface Icrud
+    {
+        bool ConectaBD();
+        bool DesconectaBD(string tabla, string condicion);
+        bool Login(string usuario, string pass);
+        bool Leer(string campos, string tabla);
+        bool Eliminar(string tabla, string donde, string id);
+        bool Actualizar(string tabla, string campo, string id, string valorid);
+        bool Insertar(string tabla, string campos, string valores);
+    }
+}
