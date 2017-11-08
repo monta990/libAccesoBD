564a58167dd570e5ee57cf586be4e683c31c3f3c
Edwin Elias Alvarez <elias986@yahoo.com>
2017-11-08 04:08:23 -0700

    CRUD multi hilo

--- libAccesoBD/Conectora.cs
+++ libAccesoBD/Conectora.cs
@@ -18,6 +18,9 @@ namespace libAccesoBD
         private string campos;
         private string tabla;
         private string valores;
+        private string donde;
+        private string id;
+        private string valorid;
         /// <summary>
         /// Guardar en archivo
         /// </summary>
@@ -34,16 +37,6 @@ namespace libAccesoBD
             Monitor.Exit(locker);
             return status;
         }
-        #region Insertar
-        public bool Insertar(string tabla, string campos, string valores)
-        {
-            bool status = true;
-            Objmysql.Insertar(tabla, campos, valores);
-            Objpostgresql.Insertar(tabla, campos, valores);
-            Objmssqlserver.Insertar(tabla, campos, valores);
-            return status;
-        }
-        #endregion Fin de insertar
         #region Leer
         /// <summary>
         /// Leer MySQL
@@ -108,21 +101,201 @@ namespace libAccesoBD
             return status;
         }
         #endregion Fin de Leer
+        #region Insertar
+        /// <summary>
+        /// Insertar MySQL
+        /// </summary>
+        public void InsertarMySQL()
+        {
+            sem.WaitOne(10);
+            Objmysql.Insertar(tabla, campos, valores);
+            Stamp("Fin de Escritura con: Hilo Escritura en MySQL " + DateTime.Now.ToLongTimeString());
+            sem.Release();
+        }
+        /// <summary>
+        /// Leer MSSQL
+        /// </summary>
+        public void InsertarMSSSQL()
+        {
+            sem.WaitOne(20);
+            Objmssqlserver.Insertar(tabla, campos, valores);
+            Stamp("Fin de Escritura con: Hilo Escritura en MSSQL " + DateTime.Now.ToLongTimeString());
+            sem.Release();
+        }
+        /// <summary>
+        /// Leer PostgreSQL
+        /// </summary>
+        public void InsertarPostgreSQL()
+        {
+            sem.WaitOne(30);
+            Objpostgresql.Insertar(tabla, campos, valores);
+            Stamp("Fin de Escritura con: Hilo Escritura en PostgreSQL " + DateTime.Now.ToLongTimeString());
+            sem.Release();
+        }
+        public bool Insertar(string tabla, string campos, string valores)
+        {
+            this.tabla = tabla;
+            this.campos = campos;
+            this.valores = valores;
+            bool status = true;
+            Stamp("");
+            Stamp("Inicio de Log de Escritura en BD");
+            Thread HiloEscrituraMySQL = new Thread(new ThreadStart(InsertarMySQL))
+            {
+                Name = "Hilo Escritura en MySQL"
+            };
+            Thread HiloEscrituraMSSQL = new Thread(new ThreadStart(InsertarMSSSQL))
+            {
+                Name = "Hilo Escritura en MSSQL"
+            };
+            Thread HiloEscrituraPostgreSQL = new Thread(new ThreadStart(InsertarPostgreSQL))
+            {
+                Name = "Hilo Escritura en PostgreSQL"
+            };
+            Stamp("Inicio de Escritura con: Hilo Escritura en MySQL " + DateTime.Now.ToLongTimeString());
+            HiloEscrituraMySQL.Start();
+            Stamp("Inicio de Escritura con: Hilo Escritura en MSSQL " + DateTime.Now.ToLongTimeString());
+            HiloEscrituraMSSQL.Start();
+            HiloEscrituraMSSQL.Join();
+            Stamp("Inicio de Escritura con: Hilo Escritura en PostgreSQL " + DateTime.Now.ToLongTimeString());
+            HiloEscrituraPostgreSQL.Start();
+            HiloEscrituraPostgreSQL.Join();
+            //Objmysql.Insertar(tabla, campos, valores);
+            //Objpostgresql.Insertar(tabla, campos, valores);
+            //Objmssqlserver.Insertar(tabla, campos, valores);
+            return status;
+        }
+        #endregion Fin de insertar
+        #region Eliminar
+        /// <summary>
+        /// Eliminar MySQL
+        /// </summary>
+        public void EliminarMySQL()
+        {
+            sem.WaitOne(10);
+            Objmysql.Eliminar(tabla, donde, id);
+            Stamp("Fin de Eliminar con: Hilo Eliminar en MySQL " + DateTime.Now.ToLongTimeString());
+            sem.Release();
+        }
+        /// <summary>
+        /// Leer MSSQL
+        /// </summary>
+        public void EliminarMSSSQL()
+        {
+            sem.WaitOne(20);
+            Objmssqlserver.Eliminar(tabla, donde, id);
+            Stamp("Fin de Eliminar con: Hilo Eliminar en MSSQL " + DateTime.Now.ToLongTimeString());
+            sem.Release();
+        }
+        /// <summary>
+        /// Eliminar PostgreSQL
+        /// </summary>
+        public void EliminarPostgreSQL()
+        {
+            sem.WaitOne(30);
+            Objpostgresql.Eliminar(tabla, donde, id);
+            Stamp("Fin de Eliminar con: Hilo Eliminar en PostgreSQL " + DateTime.Now.ToLongTimeString());
+            sem.Release();
+        }
         public bool Eliminar(string tabla, string donde, string id)
         {
             bool status = true;
-            Objmysql.Eliminar(tabla, donde, id);
-            Objpostgresql.Eliminar(tabla, donde, id);
-            Objmssqlserver.Eliminar(tabla, donde, id);
+            this.tabla = tabla;
+            this.donde = donde;
+            this.id = id;
+            Stamp("");
+            Stamp("Inicio de Log de Eliminar en BD");
+            Thread HiloEliminarMySQL = new Thread(new ThreadStart(EliminarMySQL))
+            {
+                Name = "Hilo Eliminar en MySQL"
+            };
+            Thread HiloEliminarMSSQL = new Thread(new ThreadStart(EliminarMSSSQL))
+            {
+                Name = "Hilo Eliminar en MSSQL"
+            };
+            Thread HiloEliminarPostgreSQL = new Thread(new ThreadStart(EliminarPostgreSQL))
+            {
+                Name = "Hilo Eliminar en PostgreSQL"
+            };
+            Stamp("Inicio de Eliminar con: Hilo Eliminar en MySQL " + DateTime.Now.ToLongTimeString());
+            HiloEliminarMySQL.Start();
+            Stamp("Inicio de Eliminar con: Hilo Eliminar en MSSQL " + DateTime.Now.ToLongTimeString());
+            HiloEliminarMSSQL.Start();
+            HiloEliminarMSSQL.Join();
+            Stamp("Inicio de Eliminar con: Hilo Eliminar en PostgreSQL " + DateTime.Now.ToLongTimeString());
+            HiloEliminarPostgreSQL.Start();
+            HiloEliminarPostgreSQL.Join();
+            //Objmysql.Eliminar(tabla, donde, id);
+            //Objpostgresql.Eliminar(tabla, donde, id);
+            //Objmssqlserver.Eliminar(tabla, donde, id);
             return status;
         }
-        public bool Actualizar(string tabla, string campo, string id, string valorid)
+        #endregion Eliminar
+        #region Actualizar
+        /// <summary>
+        /// Actualizar MySQL
+        /// </summary>
+        public void ActualizarMySQL()
+        {
+            sem.WaitOne(10);
+            Objmysql.Actualizar(tabla, campos, id, valorid);
+            Stamp("Fin de Actualizar con: Hilo Actualizar en MySQL " + DateTime.Now.ToLongTimeString());
+            sem.Release();
+        }
+        /// <summary>
+        /// Actualizar MSSQL
+        /// </summary>
+        public void ActualizarMSSSQL()
+        {
+            sem.WaitOne(20);
+            Objmssqlserver.Actualizar(tabla, campos, id, valorid);
+            Stamp("Fin de Actualizar con: Hilo Actualizar en MSSQL " + DateTime.Now.ToLongTimeString());
+            sem.Release();
+        }
+        /// <summary>
+        /// Leer PostgreSQL
+        /// </summary>
+        public void ActualizarPostgreSQL()
+        {
+            sem.WaitOne(30);
+            Objpostgresql.Actualizar(tabla, campos, id, valorid);
+            Stamp("Fin de Actualizar con: Hilo Actualizar en PostgreSQL " + DateTime.Now.ToLongTimeString());
+            sem.Release();
+        }
+        public bool Actualizar(string tabla, string campos, string id, string valorid)
         {
             bool status = true;
-            Objmysql.Actualizar(tabla,campo,id,valorid);
-            Objpostgresql.Actualizar(tabla, campo, id, valorid);
-            Objmssqlserver.Actualizar(tabla, campo, id, valorid);
+            this.tabla = tabla;
+            this.campos = campos;
+            this.id = id;
+            this.valorid = valorid;
+            //Objmysql.Actualizar(tabla,campos,id,valorid);
+            //Objpostgresql.Actualizar(tabla, campos, id, valorid);
+            //Objmssqlserver.Actualizar(tabla, campos, id, valorid);
+            Stamp("");
+            Stamp("Inicio de Log de Actualizar en BD");
+            Thread HiloActualizarMySQL = new Thread(new ThreadStart(ActualizarMySQL))
+            {
+                Name = "Hilo Actualizar en MySQL"
+            };
+            Thread HiloActualizarMSSQL = new Thread(new ThreadStart(ActualizarMSSSQL))
+            {
+                Name = "Hilo Actualizar en MSSQL"
+            };
+            Thread HiloActualizarPostgreSQL = new Thread(new ThreadStart(ActualizarPostgreSQL))
+            {
+                Name = "Hilo Actualizar en PostgreSQL"
+            };
+            Stamp("Inicio de Actualizar con: Hilo Actualizar en MySQL " + DateTime.Now.ToLongTimeString());
+            HiloActualizarMySQL.Start();
+            Stamp("Inicio de Actualizar con: Hilo Actualizar en MSSQL " + DateTime.Now.ToLongTimeString());
+            HiloActualizarMSSQL.Start();
+            HiloActualizarMSSQL.Join();
+            Stamp("Inicio de Actualizar con: Hilo Actualizar en PostgreSQL " + DateTime.Now.ToLongTimeString());
+            HiloActualizarPostgreSQL.Start();
+            HiloActualizarPostgreSQL.Join();
             return status;
         }
+        #endregion Actualizar
     }
 }
